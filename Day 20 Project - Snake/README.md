b7a3d7b21ff6a3c4955b2bfe9a96bfe288c3a96f
Ashutosh Vaidya <ashutosh.vaidya1190@gmail.com>
2023-07-24 14:34:40 +0530

    Day 20
    Snake basic setup

---
+++ Day 20 Project - Snake/README.md
@@ -0,0 +1,13 @@
+## Simple Snake Game
+
+### Day 20 Goal:
+- [X] Create a Snake Body
+- [X] Move the Snake
+- [ ] Control the Snake
+
+### Day 21 Goal
+- [ ] Detect collision with food
+- [ ] Create a scoreboard
+- [ ] Detect a collision with wall
+- [ ] Detect a collision with tail
+
---
+++ Day 20 Project - Snake/snake.py
@@ -0,0 +1,48 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVEMENT = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+SHAPE = "square"
+COLOR = "white"
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for pos in STARTING_POSITIONS:
+            new_segments = Turtle(SHAPE)
+            new_segments.color(COLOR)
+            new_segments.penup()
+            new_segments.goto(pos)
+            self.segments.append(new_segments)
+
+    def move(self):
+        for seg_index in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_index - 1].xcor()
+            new_y = self.segments[seg_index - 1].ycor()
+            self.segments[seg_index].goto(new_x, new_y)
+        self.head.forward(MOVEMENT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
