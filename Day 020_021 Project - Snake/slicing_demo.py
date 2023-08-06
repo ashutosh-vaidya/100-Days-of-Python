11e4697ba26d66bd8357080b206c6e0311371417
Ashutosh Vaidya <ashutosh.vaidya1190@gmail.com>
2023-08-06 09:55:03 +0530

    delete

--- Day 020_021 Project - Snake/slicing_demo.py
+++
@@ -1,5 +0,0 @@
-piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
-piano_tuple = ("do", "re", "mi", "fa", "so", "la", "ti")
-
-print(piano_tuple[1:])  # [start_index:end_index]
-print(piano_keys[2::2])  # [start_index:end_index:step(jump)]
