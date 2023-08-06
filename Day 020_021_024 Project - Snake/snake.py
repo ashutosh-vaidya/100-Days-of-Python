1756cb0e541ca100aaabf51b2e957d76ea960ce9
Ashutosh Vaidya <ashutosh.vaidya1190@gmail.com>
2023-08-06 09:53:22 +0530

    New Feature = Highscore

---
+++ Day 020_021_024 Project - Snake/snake.py
@@ -0,0 +1,61 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVEMENT = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+SHAPE = "square"
+COLOR = "white"
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for pos in STARTING_POSITIONS:
+            self.add_segment(pos)
+
+    def add_segment(self, pos):
+        new_segments = Turtle(SHAPE)
+        new_segments.color(COLOR)
+        new_segments.penup()
+        new_segments.goto(pos)
+        self.segments.append(new_segments)
+        
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def move(self):
+        for seg_index in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_index - 1].xcor()
+            new_y = self.segments[seg_index - 1].ycor()
+            self.segments[seg_index].goto(new_x, new_y)
+        self.head.forward(MOVEMENT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
